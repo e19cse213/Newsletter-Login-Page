4dcf90e3194d9213dbb125bc4e019aa139927930
Anmol Kaloni <e19cse213@bennett.edu.in>
2023-04-05 13:08:00 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// jshint esversion:13
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request")
+const https = require("https");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html")
+})
+ 
+
+app.post("/",function(req,res){
+
+    const fn = req.body.fname;
+    const ln = req.body.lname;
+    const eml = req.body.email;
+
+    // console.log(fn,ln,eml);
+    const data = {
+        members: [
+            {
+                email_address: eml,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME:fn,
+                    LNAME:ln
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data)
+   
+    const url = "https://us14.api.mailchimp.com/3.0/lists/0339f6538e";
+    const options = {
+        method:"POST",
+        auth:"anmol:918237c91528ff71f5e03bacbf53771c-us14"
+    }
+    const request = https.request(url , options, function(response){
+
+        if(response.statusCode===200){
+            
+                res.sendFile(__dirname + "/success.html")
+            
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data))
+        })
+    })
+
+    request.write(jsonData);
+    request.end()
+
+})
+
+
+app.post("/failure",function(req,res){
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT,function(){
+    console.log("Server is running on port 3000");
+})
+
+
+// API KEY 
+// 918237c91528ff71f5e03bacbf53771c-us14
+
+// List ID
+// 0339f6538e
